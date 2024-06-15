42df236057c728d4bdaa105dc16b6219ebf3f209
Nathan Albuquerque <nathan.nra@outlook.com>
2024-06-15 00:01:21 -0300

    feat: entidade Categoria criada

---
+++ app/Models/Categoria.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nome',
+    ];
+}
